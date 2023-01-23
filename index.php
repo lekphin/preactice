b8ee60bace3901caa8b78ce28c0e2cee1de5e90b
lekphin <phinlek20@gamil.com>
2023-01-23 14:37:11 +0700

    ujgyu

--- index.php
+++ index.php
@@ -9,79 +9,74 @@
 </head>
 <body>
     <div class="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center  py-5">
-        <!-- <div class="bg-gray-100 text-gray-500  w-full outlne-0 " style="max-width:1000px"> -->
-            <div class="md:flex w-full h-2/4">
-                <!-- <div class="">
-                    <img src="image/register-img.png" alt="">
-                </div> -->
-                <div class="w-full md:w-1/2 py-10  md:px-10 m-auto " style="background-color:#133954;">
-                    <div class="text-center mb-3">
-                        <h1 class="font-bold text-3xl text-white">Register</h1>
-                    </div>
-                    <div>
-                        <div class="flex -mx-3">
-                            <div class="w-full px-3 mb-1">
-                                <label for="" class="text-white font-semibold px-1 font-medium">Username</label>
-                                <div class="flex">
-                                    <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                                    <input type="text" class="w-full -ml-10 p-1.5 border-2 border-gray-200 outline-none focus:border-indigo-500 font-thin" placeholder="Enter your username">
+        <div class="md:flex w-full h-2/4">
+            <div class="w-full md:w-1/2 py-10  md:px-10 m-auto" style="background-color:#133954;">
+                <div class="text-center mb-3">
+                    <h1 class="font-bold text-3xl text-white">REGISTER</h1>
+                </div>
+                <div>
+                    <div class="flex -mx-3">
+                        <div class="w-full px-3 mb-5">
+                            <label for="" class="text-white font-semibold px-1 font-medium">Username</label>
+                            <div class="flex">
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
+                                <input type="text" class="w-full -ml-10 p-1.5 border-2 border-gray-200 outline-none  font-thin" placeholder="Enter your username">
                                 </div>
                             </div>
                         </div>
                         <div class="flex -mx-3">
-                            <div class="w-full px-3 mb-1">
+                            <div class="w-full px-3 mb-5">
                                 <label for="" class="text-white font-semibold px-1 font-medium">Email</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                                    <input type="email" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none focus:border-indigo-500 font-thin" placeholder="Enter your email">
+                                    <input type="email" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none  font-thin" placeholder="Enter your email">
                                 </div>
                             </div>
                         </div>
                         <div class="flex -mx-3">
-                            <div class="w-full px-3 mb-1">
+                            <div class="w-full px-3 mb-5">
                                 <label for="" class="text-white font-semibold px-1 font-medium">Password</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-email-outline text-gray-400 text-lg"></i></div>
-                                    <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none focus:border-indigo-500 font-thin" placeholder="Enter your password">
+                                    <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your password">
                                 </div>
                             </div>
                         </div>
                         <div class="flex -mx-3">
-                            <div class="w-full px-3 mb-1">
+                            <div class="w-full px-3 mb-5">
                                 <label for="" class="text-white  font-semibold px-1 font-medium"> Confirm  password</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                    <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none focus:border-indigo-500 font-thin" placeholder="Enter your confirm password">
+                                    <input type="password" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your confirm password">
                                 </div>
                             </div>
                         </div>
                         <div class="flex -mx-3">
-                            <div class="w-full px-3 mb-1">
-                                <label for="" class="text-white  font-semibold px-1 font-medium">Phone number</label>
+                            <div class="w-full px-3 mb-5">
+                                <label for="" class="text-white  font-semibold px-1 font-medium ">Phone number</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                    <input type="number" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none focus:border-indigo-500 font-thin" placeholder="Enter your phone number">
+                                    <input type="number" class="w-full -ml-10  pr-3  p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your phone number">
                                 </div>
                             </div>
                         </div>
                         <div class="flex -mx-3">
-                            <div class="w-full px-3 mb-10">
+                            <div class="w-full px-3 mb-9">
                                 <label for="" class="text-white  font-semibold px-1 font-medium text-blak-500">Address</label>
                                 <div class="flex">
                                     <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
-                                    <input type="text" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none focus:border-indigo-500 font-thin" placeholder="Enter your address ">
+                                    <input type="text" class="w-full -ml-10 pr-3 p-1.5  border-2 border-gray-200 outline-none font-thin" placeholder="Enter your address ">
                                 </div>
                             </div>
                         </div>
                         <div class="flex -mx-3">
-                            <div class="w-full px-3 mb-3">
-                                <button class="block w-full  mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white  px-3 p-1.5 font-semibold">REGISTER NOW</button>
+                            <div class="w-full px-3 mb-5">
+                                <button class="block w-full  mx-auto bg-indigo-500  text-white  px-3 p-1.5 font-semibold" style="background-color:#185B8B;">Register now</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        <!-- </div> -->
     </div>
 </body>
 </html>
